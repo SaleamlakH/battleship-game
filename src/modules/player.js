aa49798fba2ab5e4fcff670a86e1bc68d73e73d9
SaleamlakH <saleamlak.7h@gmail.com>
2026-02-23 22:44:27 +0300

    feat(player): add placeRandomly method

--- src/modules/player.js
+++ src/modules/player.js
@@ -1,4 +1,4 @@
-import { GameBoard } from './game-board';
+import { GameBoard, DIRECTION_VECTORS } from './game-board';
 
 export class Player {
   #gameBoard;
@@ -9,5 +9,36 @@ export class Player {
 
   get gameBoard() {
     return this.#gameBoard;
+  }
+
+  placeRandomly() {
+    const directions = Object.keys(DIRECTION_VECTORS);
+    const placement = [];
+
+    this.#gameBoard.fleet.forEach((ship) => {
+      let success = false;
+      while (!success) {
+        const direction =
+          directions[Math.floor(Math.random() * directions.length)];
+
+        const coordinate = [
+          Math.floor(Math.random() * 10) + 1,
+          Math.floor(Math.random() * 10) + 1,
+        ];
+
+        const result = this.#gameBoard.placeShip(ship, coordinate, direction);
+
+        success = result.success;
+        if (success) {
+          placement.push({
+            type: ship.type,
+            size: ship.size,
+            coordinates: result.coordinates,
+          });
+        }
+      }
+    });
+
+    return placement;
   }
 }
---
+++ tests/player.test.js
@@ -0,0 +1,35 @@
+import { Player } from '../src/modules/player';
+
+describe('Player', () => {
+  describe('Place randomly', () => {
+    let player, gameBoard, placement;
+    beforeEach(() => {
+      player = new Player();
+      gameBoard = player.gameBoard;
+      placement = player.placeRandomly();
+    });
+
+    test('Return placement object', () => {
+      expect(placement).toBeDefined();
+    });
+
+    test('Number coordinates in placement matches total fleet size', () => {
+      expect(placement.length).toBe(gameBoard.fleet.length);
+    });
+
+    test('the number of coordinates of a ship matches ship size', () => {
+      placement.forEach(({ size, coordinates }) => {
+        expect(coordinates.length).toBe(size);
+      });
+    });
+
+    test('All coordinates in the direction has expected ship', () => {
+      placement.forEach(({ type, coordinates }) => {
+        coordinates.forEach((coordinate) => {
+          expect(gameBoard.get(coordinate)).toBeDefined();
+          expect(gameBoard.get(coordinate).type).toBe(type);
+        });
+      });
+    });
+  });
+});
